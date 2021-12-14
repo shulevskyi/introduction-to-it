b4ffe58f3087ed5ba2988279e407849458f1a1de
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-14 19:31:06 +0200

    Create 1-step.js

---
+++ lecture_notes/10 refactoring/1-step.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Function that iterates each element in polyline checks if it's an obj; parse it using JSON.
+const PointParse = (points) => {
+    return points.map((point) => {
+        if (typeof point != 'object') {
+            return JSON.parse(point);
+        } else {
+            return point
+        }
+    });
+}
+
+// Taking a result from PointParse and shifting it
+const PointsShift = (arr, dot_param) => {
+    return arr.map((dot) => {
+        dot['x'] += dot_param['x']
+        dot['y'] += dot_param['y']
+        return dot;
+    });
+}
+
+const polyline = [
+    { x: 0, y: 0 },
+    { x: 10, y: 10 },
+    '{ "x": 20, "y": 20 }',
+    { x: 30, y: 30 },
+];
+
+
+const path = PointParse(polyline)
+console.log(PointsShift(path, { x: 10, y: -5 }))
+
