4d0a274b1e867e17e02153a702c8fdeb245ca5ca
shulevskyi <90912603+shulevskyi@users.noreply.github.com>
2021-12-22 12:43:34 +0200

    Update 1-not-optimal.js

--- lecture_notes/10 refactoring/1-not-optimal.js
+++ lecture_notes/10 refactoring/1-not-optimal.js
@@ -1,19 +1,24 @@
 'use strict';
 
-let movePoints = (offset, points) => {
-  points.forEach((point) => {
-    const type = typeof point;
-    if (type === 'object') {
-      point.x += offset.x;
-      point.y += offset.y;
+const pointParse = (points) => {
+  points.map((point) => {
+    if (typeof point !== 'object') {
+      return JSON.parse(point);
     } else {
-      let i = points.indexOf(point);
-      points[i] = JSON.parse(point);
-      points[i].x += offset.x;
-      points[i].y += offset.y;
+      return point;
     }
   });
   return points;
+};
+
+// Taking a result from PointParse and shifting it
+const pointsShift = (arr, dotParam) => {
+  arr.map((dot) => {
+    dot['x'] += dotParam['x'];
+    dot['y'] += dotParam['y'];
+    return dot;
+  });
+  return arr;
 };
 
 const polyline = [
@@ -23,5 +28,5 @@ const polyline = [
   { x: 30, y: 30 },
 ];
 
-movePoints({ x: 10, y: -5 }, polyline);
-console.log({ polyline });
+const path = pointParse(polyline);
+console.log(pointsShift(path, { x: 10, y: -5 }));
